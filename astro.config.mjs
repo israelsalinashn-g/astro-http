03626112baef113c7e960e186051702ae9b7d667
Israel Salinas <israelsalinashn@gmail.com>
2024-10-06 22:56:22 -0600

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  integrations: [mdx(), sitemap()],
+
+  output: 'server',
+  adapter: cloudflare(),
 });
